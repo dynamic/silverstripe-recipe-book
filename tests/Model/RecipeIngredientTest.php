65b0866bd08c3f14b10be1ce98395e5a2fe16ae6
Nic Horstmeier <nhorstmeier@dynamicagency.com>
2021-08-31 20:52:31 -0500

    BUGFIX default sort value not set when creating ingredient/direction

--- tests/Model/RecipeIngredientTest.php
+++ tests/Model/RecipeIngredientTest.php
@@ -3,8 +3,10 @@
 namespace Dynamic\RecipeBook\Test\Model;
 
 use Dynamic\RecipeBook\Model\RecipeIngredient;
+use Dynamic\RecipeBook\Page\RecipePage;
 use SilverStripe\Dev\SapphireTest;
 use SilverStripe\Forms\FieldList;
+use SilverStripe\ORM\ValidationException;
 
 class RecipeIngredientTest extends SapphireTest
 {
@@ -21,5 +23,33 @@ class RecipeIngredientTest extends SapphireTest
         $object = $this->objFromFixture(RecipeIngredient::class, 'one');
         $fields = $object->getCMSFields();
         $this->assertInstanceOf(FieldList::class, $fields);
+    }
+
+    /**
+     * @throws ValidationException
+     */
+    public function testDefaultSortValue()
+    {
+        $recipe = $this->objFromFixture(RecipePage::class, 'two');
+
+        $ingredient = RecipeIngredient::create();
+        $ingredient->Title = 'My First Ingredient';
+        $ingredient->RecipeID = $recipe->ID;
+
+        $this->assertEquals(0, $ingredient->Sort);
+
+        $ingredient->write();
+
+        $this->assertNotEquals(0, $ingredient->Sort);
+
+        $ingredient2 = RecipeIngredient::create();
+        $ingredient2->Title = 'My Second Ingredient';
+        $ingredient2->RecipeID = $recipe->ID;
+
+        $this->assertEquals(0, $ingredient2->Sort);
+
+        $ingredient2->write();
+
+        $this->assertGreaterThan($ingredient->Sort, $ingredient2->Sort);
     }
 }
